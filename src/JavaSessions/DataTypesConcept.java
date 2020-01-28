6d1cc67c5dc95dd8efcfedbd3df0fdf304ef426c
MINKU BHATLIA <minkubhatlia@MINKUs-Air.lan>
2020-01-28 10:41:55 +0000

    adding my first commitment

---
+++ src/JavaSessions/DataTypesConcept.java
@@ -0,0 +1,14 @@
+package JavaSessions;
+
+public class DataTypesConcept {
+
+	public static void main(String[] args) {
+		
+		int i = 20;
+		int j = 30;
+		int k = i+j;
+		System.out.println(k);
+	
+	}
+
+}
